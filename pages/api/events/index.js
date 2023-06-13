a8e42556de9d9880c91e8e274675094f52a4d85f
Milosz Bauman <milosz.bauman@sensilabs.pl>
2023-06-14 00:43:32 +0200

    Fixed pending state in SSE handler

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -3,20 +3,30 @@
 
 import Recipe from "../../../models/recipe";
 
+import { NextRequest, NextResponse } from 'next/server';
+;
+
+export const config = {
+    runtime: 'edge',
+};
+
 export default async function handler (req, res)  {
     try {
-        res.writeHead(200, {
-            Connection: 'keep-alive',
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'text/event-stream',
-            'Content-Encoding': 'none'
-        });
 
-         Recipe.watch().on('change', data => {
-             const d = JSON.stringify({data});
-             res.write(`id: ${(new Date()).toLocaleTimeString()}\ndata: ${d}\n\n`);
+        // res.writeHead(200, {
+        //     Connection: 'keep-alive',
+        //     'Cache-Control': 's-maxage=600, stale-while-revalidate=30',
+        //     'Content-Type': 'text/event-stream',
+        //     'Content-Encoding': 'none'
+        // });
 
-         });
+        res.write('sse')
+
+         // Recipe.watch().on('change', data => {
+         //     const d = JSON.stringify({data});
+         //     res.write(`id: ${(new Date()).toLocaleTimeString()}\ndata: ${d}\n\n`);
+         //
+         // });
 
     } catch (e) {
         new Response('Failed to fetch', {status: 500})
